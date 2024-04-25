d1c81655e487d6f38d2542ac3e6b37cb9c2a8a97
marulesh27 <167081252+marulesh27@users.noreply.github.com>
2024-04-25 11:52:18 +0530

    demonstration of selection sort algorithm

---
+++ Selection Sort.py
@@ -0,0 +1,26 @@
+
+n=int(input())
+nums=list(map(int,input().split()))
+n = len(nums)
+def performSelectionSort(nums):
+    n = len(nums)
+   
+    for fixThisIndex in range(n - 1, 0, -1):
+        
+        maxEle = nums[fixThisIndex]
+        maxEleIndex = fixThisIndex 
+ 
+        for index in range(fixThisIndex):
+         
+            if nums[index] > maxEle:
+                maxEleIndex = index 
+                maxEle = nums[index]
+        if fixThisIndex != maxEleIndex:
+            temp = nums[maxEleIndex]
+            nums[maxEleIndex] = nums[fixThisIndex]
+            nums[fixThisIndex] = temp
+ 
+        
+performSelectionSort(nums)
+
+print(*nums)
