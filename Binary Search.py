7b1652f0c0d5f8922a716e5a29635c2cc465895c
marulesh27 <167081252+marulesh27@users.noreply.github.com>
2024-04-29 16:14:14 +0530

    Binary Search Demo

---
+++ Binary Search.py
@@ -0,0 +1,22 @@
+n = int(input())
+l = list(map(int, input().split()))
+target = int(input())
+
+left = 0
+right = n - 1
+found = False
+
+while left <= right:
+   mid = (left + right) // 2
+   if l[mid] == target:
+       found = True
+       break
+   elif l[mid] > target:
+       right = mid - 1
+   else:
+       left = mid + 1
+      
+if found == True:
+   print("Target is present")
+else:
+   print("Target is not present")
