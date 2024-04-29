9e7b902861678e141ffa2dd307a0c3bab9618a6d
marulesh27 <167081252+marulesh27@users.noreply.github.com>
2024-04-29 16:00:05 +0530

    Kth smallest Element in BST

---
+++ Kth smallest.py
@@ -0,0 +1,35 @@
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+def insert(root, val):
+    if root is None:
+        return TreeNode(val)
+    if val < root.val:
+        root.left = insert(root.left, val)
+    elif val > root.val:
+        root.right = insert(root.right, val)
+    return root
+
+def kth_smallest(root, k):
+    stack = []
+    while True:
+        while root:
+            stack.append(root)
+            root = root.left
+        root = stack.pop()
+        k -= 1
+        if k == 0:
+            return root.val
+        root = root.right
+
+n = int(input())
+elements = list(map(int, input().split()))
+k = int(input())
+root = None
+for element in elements:
+    root = insert(root, element)
+result = kth_smallest(root, k)
+print(result)
